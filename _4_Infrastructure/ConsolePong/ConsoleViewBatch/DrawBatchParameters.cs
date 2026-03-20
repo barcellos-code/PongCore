ab9c9f233005781b2dfa8fb0467e541d257fdd04
Vitor Barcellos <vtor.barcellos7@gmail.com>
2026-03-20 15:34:11 -0300

    Remove ConsolePong files, which  now live in a different repo

--- _4_Infrastructure/ConsolePong/ConsoleViewBatch/DrawBatchParameters.cs
+++
@@ -1,8 +0,0 @@
-namespace ConsoleViewBatch;
-
-public readonly struct DrawBatchParameters(int instanceGUID, Action drawAction, bool isOneTimeDraw)
-{
-    public readonly int InstanceGUID = instanceGUID;
-    public readonly Action DrawAction = drawAction;
-    public readonly bool IsOneTimeDraw = isOneTimeDraw;
-}
--- _4_Infrastructure/ConsolePong/ConsoleViewBatch/IViewBatch.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsoleViewBatch;
-
-public interface IViewBatch
-{
-    void QueueForDraw(DrawBatchParameters parameters);
-    void UnqueueForDraw(int instanceGUID);
-    void Start();
-    void Stop();
-}
